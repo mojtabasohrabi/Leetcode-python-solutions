5cc3d1d4f1cd45275a41ec38c5c11e19c35690b2
mojtaba <dev.mojtabasohrabi@gmail.com>
2025-05-22 19:08:46 +0330

    feat: write solution of 1.two-sum problem

--- easy/1-two-sum.py
+++ easy/1-two-sum.py
@@ -1,12 +1,12 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dictionary = {}
-        for i in nums:
-            value_to_find = target - i
-            if value_to_find in dictionary.keys():
-                return [nums.index(value_to_find), nums.index(dictionary[value_to_find], nums.index(value_to_find) + 1)]
-            dictionary[i] = value_to_find
-        return []
+def two_sum(nums: List[int], target: int) -> List[int]:
+    dictionary = {}
+    for i in nums:
+        value_to_find = target - i
+        if value_to_find in dictionary.keys():
+            return [nums.index(value_to_find), nums.index(dictionary[value_to_find], nums.index(value_to_find) + 1)]
+        dictionary[i] = value_to_find
+    return []
+
 
 """
 this solution was accepted and get this results:
