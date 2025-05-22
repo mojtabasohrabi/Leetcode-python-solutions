0cb08af1bb63d9f76c0204938e07d5c8cab5211a
mojtaba <dev.mojtabasohrabi@gmail.com>
2025-05-22 19:08:46 +0330

    feat: write solution of 1.two-sum problem

---
+++ easy/1-two-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dictionary = {}
+        for i in nums:
+            value_to_find = target - i
+            if value_to_find in dictionary.keys():
+                return [nums.index(value_to_find), nums.index(dictionary[value_to_find], nums.index(value_to_find) + 1)]
+            dictionary[i] = value_to_find
+        return []
+
+"""
+this solution was accepted and get this results:
+
+Runtime: 0 ms (Beats: 100%)
+
+Memory: 17.89 mb (Beats: 99.95%)
+"""
