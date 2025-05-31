845884ca44fb20e0b0ca7f96ca17ac826875757d
mojtabasohrabi <dev.mojtabasohrabi@gmail.com>
2025-05-31 18:54:59 +0330

    feat: write solution of 70-Climbing-Stairs problem

---
+++ easy/70-Climbing-Stairs.py
@@ -0,0 +1,22 @@
+def climb_stairs(self, n: int) -> int:
+    if n <= 1:
+        return 1
+
+    first, second = 1, 2
+    if n == 1:
+        return first
+    if n == 2:
+        return second
+
+    for _ in range(3, n + 1):
+        first, second = second, first + second
+    return second
+
+
+"""
+this solution was accepted and get this results:
+
+Runtime: 0 ms (Beats: 100%)
+
+Memory: 18.00 mb (Beats: 07.91%)
+"""
